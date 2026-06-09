3408452457cd18c83b3441b190528b8864c65686
Zunaid <133950183+ZunaidChowdhury@users.noreply.github.com>
2026-06-09 20:25:52 +0600

    disabled dns code

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import dns from "node:dns/promises";
-dns.setServers(["8.8.8.8", "1.1.1.1"]);
+// import dns from "node:dns/promises";
+// dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 import express from "express";
 import cors from "cors";
